c7bd22232d99f7da4eb4d8202115148fe15f354c
jaingopal <jaingopal1980@gmail.com>
2026-06-23 19:02:22 +0530

    Changed the github name inside

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,25 +1,25 @@
 import { products } from "../data/product.js";
-import {renderPage,searched_products} from "./grid.js"
-let last_searched='';
+import { renderPage, searched_products } from "./grid.js"
+let last_searched = '';
 
-function find(name,key){
-    let i=1,temp=-1;
-    const kmp=[-1];
-    while(i<key.length){
-        if(key[i]===key[temp+1]){
+function find(name, key) {
+    let i = 1, temp = -1;
+    const kmp = [-1];
+    while (i < key.length) {
+        if (key[i] === key[temp + 1]) {
             temp++;
             kmp.push(temp);
         }
-        else{
-            while(temp!=-1&&key[i]!=key[temp+1]){
-                temp=kmp[temp];
+        else {
+            while (temp != -1 && key[i] != key[temp + 1]) {
+                temp = kmp[temp];
             }
-            if(temp!=-1){
+            if (temp != -1) {
                 temp++;
                 kmp.push(temp);
             }
-            else{
-                if(key[0]===key[i]){
+            else {
+                if (key[0] === key[i]) {
                     temp++;
                 }
                 kmp.push(temp);
@@ -27,57 +27,57 @@ function find(name,key){
         }
         i++;
     }
-    i=0;
-    let j=0;
-    let len=key.length;
-    while(i<name.length){
-        if(name[i]===key[j]){
+    i = 0;
+    let j = 0;
+    let len = key.length;
+    while (i < name.length) {
+        if (name[i] === key[j]) {
             i++;
             j++;
-            if(j===len){
+            if (j === len) {
                 return true;
             }
             continue;
         }
-        while(j!=0&&name[i]!=key[j]){
-            j=kmp[j-1]+1;
+        while (j != 0 && name[i] != key[j]) {
+            j = kmp[j - 1] + 1;
         }
-        if(name[i]===key[j]){
+        if (name[i] === key[j]) {
             i++;
             j++;
         }
-        else{
+        else {
             i++;
-            j=0;
+            j = 0;
         }
     }
     return false;
 }
 
-export function searchWithKey(searched){
-    if(!searched){
-        window.location.assign("https://jaingopal07.github.io/ecommerce-clone/");
+export function searchWithKey(searched) {
+    if (!searched) {
+        window.location.assign("https://jaingopal.github.io/ecommerce-clone/");
         return;
     }
-    searched_products.length=0;
-    products.forEach((productElement)=>{
-        const pdt_name=productElement.name.toLowerCase();
-        if(find(pdt_name,searched)){
+    searched_products.length = 0;
+    products.forEach((productElement) => {
+        const pdt_name = productElement.name.toLowerCase();
+        if (find(pdt_name, searched)) {
             searched_products.push(productElement);
         }
     })
-    
+
     const newUrl = `${window.location.pathname}?search=${encodeURIComponent(searched)}`;
     window.history.pushState({}, '', newUrl);
     renderPage();
 }
 
-function search(){
-    const searched=navbarElement.querySelector('.search-bar').value.toLowerCase();
-    if(last_searched===searched){
+function search() {
+    const searched = navbarElement.querySelector('.search-bar').value.toLowerCase();
+    if (last_searched === searched) {
         return;
     }
-    last_searched=searched;
+    last_searched = searched;
     searchWithKey(searched);
 }
 
@@ -87,24 +87,24 @@ navbarElement.addEventListener('click', (e) => {
     if (e.target.closest('.search')) {
         search();
     }
-    else if(e.target.classList.contains('hamburger-menu-header')){
-        if(document.querySelector('.hamburger-menu-dropdown').style.display==='grid'){
-            document.querySelector('.hamburger-menu-dropdown').style.display='none';
+    else if (e.target.classList.contains('hamburger-menu-header')) {
+        if (document.querySelector('.hamburger-menu-dropdown').style.display === 'grid') {
+            document.querySelector('.hamburger-menu-dropdown').style.display = 'none';
         }
-        else{
-            document.querySelector('.hamburger-menu-dropdown').style.display='grid';
+        else {
+            document.querySelector('.hamburger-menu-dropdown').style.display = 'grid';
         }
-        
+
     }
 });
 
-navbarElement.addEventListener('keydown',(e)=>{
-    if(e.target.id==='search-bar'){
-        if(e.key==='Enter'){
+navbarElement.addEventListener('keydown', (e) => {
+    if (e.target.id === 'search-bar') {
+        if (e.key === 'Enter') {
             search();
         }
-        else if(e.key==='Escape'){
-            document.getElementById('search-bar').value='';
+        else if (e.key === 'Escape') {
+            document.getElementById('search-bar').value = '';
         }
     }
 })
